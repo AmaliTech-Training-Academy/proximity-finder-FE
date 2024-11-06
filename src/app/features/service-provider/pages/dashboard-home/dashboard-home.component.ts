a8df0edd513d383297c4eea25865565d5a9c3400
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-11-06 20:18:36 +0000

    Adds icon assets and styles dashboard header

--- src/app/features/service-provider/pages/dashboard-home/dashboard-home.component.ts
+++ src/app/features/service-provider/pages/dashboard-home/dashboard-home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+import { SidebarComponent } from '../../components/sidebar/sidebar.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard-home',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, SidebarComponent, RouterOutlet],
   templateUrl: './dashboard-home.component.html',
-  styleUrl: './dashboard-home.component.sass'
+  styleUrl: './dashboard-home.component.sass',
 })
-export class DashboardHomeComponent {
-
-}
+export class DashboardHomeComponent {}
