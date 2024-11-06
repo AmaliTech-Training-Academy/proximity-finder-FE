e0cedc819dddfc6da9f2f81d2685c0a0016fe1a8
IT Compliance <rhodaarthur48@gmail.com>
2024-11-06 14:52:47 +0000

    feat: adds profile management project structure

---
+++ src/app/features/profile-management/components/admin-profile-info/admin-profile-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-profile-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-profile-info.component.html',
+  styleUrl: './admin-profile-info.component.sass'
+})
+export class AdminProfileInfoComponent {
+
+}
