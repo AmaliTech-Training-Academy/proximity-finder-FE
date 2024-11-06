912f6dd404df1781f2790396d89376d1d95d92fb
Harr-izle <harrietbuadee@gmail.com>
2024-11-06 13:52:36 +0000

    creates component for role selection during registration

---
+++ src/app/features/auth/components/role-select/role-select.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-role-select',
+  standalone: true,
+  imports: [],
+  templateUrl: './role-select.component.html',
+  styleUrl: './role-select.component.sass'
+})
+export class RoleSelectComponent {
+
+}
