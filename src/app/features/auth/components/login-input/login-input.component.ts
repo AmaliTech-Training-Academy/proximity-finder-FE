b1f8d0065780ee47bb80e5c19cef5b8e2d967e3c
Harr-izle <harrietbuadee@gmail.com>
2024-11-07 13:34:06 +0000

    creates component for login inputs

---
+++ src/app/features/auth/components/login-input/login-input.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-input',
+  standalone: true,
+  imports: [],
+  templateUrl: './login-input.component.html',
+  styleUrl: './login-input.component.sass'
+})
+export class LoginInputComponent {
+
+}
