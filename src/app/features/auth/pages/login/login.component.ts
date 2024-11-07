b4b9a07dfa6a20176e120d9ffd925b74519b58bc
Harr-izle <harrietbuadee@gmail.com>
2024-11-07 14:20:09 +0000

    rednders login component on the login page

--- src/app/features/auth/pages/login/login.component.ts
+++ src/app/features/auth/pages/login/login.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { LoginInputComponent } from "../../components/login-input/login-input.component";
+import { CommentsComponent } from "../../components/comments/comments.component";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [LoginInputComponent, CommentsComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.sass'
 })
