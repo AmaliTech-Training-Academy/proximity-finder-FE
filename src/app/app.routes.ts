18aafdd95cc94799392c72867910d8047071a289
Harr-izle <harrietbuadee@gmail.com>
2024-11-07 13:31:11 +0000

    adds routes for various signup pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    //auth
+    {
+        path:'role-select',
+        loadComponent: () => import ('./features/auth/pages/role/role.component').then(m => m.RoleComponent)
+    },
+    {
+        path:'pro-signup',
+        loadComponent: () => import ('./features/auth/pages/provider-signup/provider-signup.component').then(m => m.ProviderSignupComponent)
+    },
+    {
+       path:'provider-check',
+        loadComponent: () => import ('./features/auth/pages/provider/provider.component').then(m => m.ProviderComponent)
+    },
+    {
+        path:'client-signup',
+        loadComponent: () => import ('./features/auth/pages/signup/signup.component').then(m => m.SignupComponent)
+    }
+];
