6f789986449cb05f754ce1b2f8331ab48af2fee1
Harr-izle <harrietbuadee@gmail.com>
2024-11-07 13:27:00 +0000

    feat:creates providers input component

---
+++ src/app/features/auth/components/provider-input/provider-input.component.ts
@@ -0,0 +1,60 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-provider-input',
+  standalone: true,
+  imports: [CommonModule,ReactiveFormsModule],
+  templateUrl: './provider-input.component.html',
+  styleUrl: './provider-input.component.sass'
+})
+export class ProviderInputComponent {
+  signUpForm!: FormGroup;
+  showPassword: boolean = false;
+  showConfirmPassword: boolean = false;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.signUpForm = this.formBuilder.group({
+      businessName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(12), this.passwordValidator]],
+      confirmPassword: ['', Validators.required]
+    }, {
+      validators: this.matchPassword
+    });
+  }
+
+  passwordValidator(control: any) {
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{12,}$/;
+    return passwordRegex.test(control.value) ? null : { invalidPassword: true };
+  }
+
+  matchPassword(group: FormGroup) {
+    const password = group.get('password')?.value;
+    const confirmPassword = group.get('confirmPassword')?.value;
+
+    if (password !== confirmPassword) {
+      return { mismatchedPassword: true };
+    }
+    return null;
+  }
+
+  togglePasswordVisibility(field: 'password' | 'confirmPassword') {
+    if (field === 'password') {
+      this.showPassword = !this.showPassword;
+    } else {
+      this.showConfirmPassword = !this.showConfirmPassword;
+    }
+  }
+
+  onSubmit() {
+    if (this.signUpForm.valid) {
+      console.log('Form submitted:', this.signUpForm.value);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+}
