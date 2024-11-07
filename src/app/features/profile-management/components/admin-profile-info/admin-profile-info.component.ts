d27b84d15a539861f16c7abe8d193c5ecabbd100
IT Compliance <rhodaarthur48@gmail.com>
2024-11-07 10:17:41 +0000

    feat: add implementation for admin profile

--- src/app/features/profile-management/components/admin-profile-info/admin-profile-info.component.ts
+++ src/app/features/profile-management/components/admin-profile-info/admin-profile-info.component.ts
@@ -9,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AdminProfileInfoComponent {
 
+  // check if form is active
+  isFormActive: boolean = false;
+
+  onFocus(): void {
+    this.isFormActive = true;
+  }
 }
--- src/app/features/profile-management/pages/admin-profile/admin-profile.component.ts
+++ src/app/features/profile-management/pages/admin-profile/admin-profile.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { AdminProfileInfoComponent } from "../../components/admin-profile-info/admin-profile-info.component";
+import {MatTabsModule} from '@angular/material/tabs';
+import { ChangePasswordComponent } from "../../components/change-password/change-password.component";
 
 @Component({
   selector: 'app-admin-profile',
   standalone: true,
-  imports: [AdminProfileInfoComponent],
+  imports: [AdminProfileInfoComponent, MatTabsModule, ChangePasswordComponent],
   templateUrl: './admin-profile.component.html',
   styleUrl: './admin-profile.component.sass'
 })
