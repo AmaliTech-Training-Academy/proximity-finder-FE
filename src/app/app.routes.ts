6704783ca405d7a0dd2e45f69954b74e978de323
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-11-07 11:35:42 +0000

    Fixes sidebar routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,18 +8,56 @@ export const routes: Routes = [
     component: DashboardLayoutComponent,
     children: [
       {
-        path: '**',
-        loadComponent: () =>
-          import(
-            './features/service-provider/pages/dashboard-home/dashboard-home.component'
-          ).then((m) => m.DashboardHomeComponent),
+        path: '',
+        redirectTo: '',
+        pathMatch: 'full',
       },
+
       {
         path: '',
         loadComponent: () =>
           import(
             './features/service-provider/pages/dashboard-home/dashboard-home.component'
           ).then((m) => m.DashboardHomeComponent),
+      },
+      {
+        path: 'requests',
+        loadComponent: () =>
+          import(
+            './features/service-provider/pages/requests/requests.component'
+          ).then((m) => m.RequestsComponent),
+      },
+      {
+        path: 'projects',
+        loadComponent: () =>
+          import(
+            './features/service-provider/pages/projects/projects.component'
+          ).then((m) => m.ProjectsComponent),
+      },
+      {
+        path: 'services',
+        loadComponent: () =>
+          import(
+            './features/service-provider/pages/services/services.component'
+          ).then((m) => m.ServicesComponent),
+      },
+      {
+        path: 'scheduling',
+        loadComponent: () =>
+          import(
+            './features/service-provider/pages/scheduling/scheduling.component'
+          ).then((m) => m.SchedulingComponent),
+      },
+      {
+        path: 'appointments',
+        loadComponent: () =>
+          import(
+            './features/service-provider/pages/appointments/appointments.component'
+          ).then((m) => m.AppointmentsComponent),
+      },
+      {
+        path: '**',
+        redirectTo: '',
       },
     ],
   },
