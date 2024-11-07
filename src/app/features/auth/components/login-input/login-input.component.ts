c55217ca005caa55c26fd3486b0b3fdf89e3fb39
Harr-izle <harrietbuadee@gmail.com>
2024-11-07 15:03:25 +0000

    validates login form

--- src/app/features/auth/components/login-input/login-input.component.ts
+++ src/app/features/auth/components/login-input/login-input.component.ts
@@ -1,12 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-input',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,ReactiveFormsModule],
   templateUrl: './login-input.component.html',
   styleUrl: './login-input.component.sass'
 })
 export class LoginInputComponent {
+  loginForm!: FormGroup;
+  showPassword: boolean = false;
+
+  constructor(private formBuilder:FormBuilder) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.required, Validators.email],
+      password: ['',Validators.required]
+    });
+  }
+
+ 
+  togglePassword() {
+    this.showPassword = !this.showPassword;
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Form submitted:', this.loginForm.value);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 
 }
