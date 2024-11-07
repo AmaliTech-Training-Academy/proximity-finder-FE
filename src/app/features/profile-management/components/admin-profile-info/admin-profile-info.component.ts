8626017eadca205ae627b32ee3f98671186cca14
IT Compliance <rhodaarthur48@gmail.com>
2024-11-07 15:26:17 +0000

    feat: add delete modal design

--- src/app/features/profile-management/components/admin-profile-info/admin-profile-info.component.ts
+++ src/app/features/profile-management/components/admin-profile-info/admin-profile-info.component.ts
@@ -1,18 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { DeleteModalComponent } from '../delete-modal/delete-modal.component';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-admin-profile-info',
   standalone: true,
-  imports: [],
+  imports: [DeleteModalComponent, MatDialogModule],
   templateUrl: './admin-profile-info.component.html',
-  styleUrl: './admin-profile-info.component.sass'
+  styleUrl: './admin-profile-info.component.sass',
+  providers: [provideNativeDateAdapter()]
 })
 export class AdminProfileInfoComponent {
+  readonly dialog = inject(MatDialog)
 
   // check if form is active
-  isFormActive: boolean = false;
+  isFormActive: boolean = false
 
   onFocus(): void {
-    this.isFormActive = true;
+    this.isFormActive = true
+  }
+
+  // display delete modal
+  openDialog(){
+    const dialogRef = this.dialog.open(DeleteModalComponent)
+    dialogRef.afterClosed().subscribe((results) => console.log(results))
   }
 }
--- src/app/features/profile-management/components/delete-modal/delete-modal.component.ts
+++ src/app/features/profile-management/components/delete-modal/delete-modal.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Inject, Output } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  MatDialogModule,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-delete-modal',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatButtonModule, MatDialogModule],
   templateUrl: './delete-modal.component.html',
   styleUrl: './delete-modal.component.sass'
 })
 export class DeleteModalComponent {
+  constructor(
+    private dialogRef: MatDialogRef<DeleteModalComponent>,
+    @Inject(MAT_DIALOG_DATA)
+    public data: {
+      title: string;
+      message: string;
+      type: string;
+      confirmText: string;
+      cancelText: string;
+    },
+  ) {}
+  
+  @Output() confirm = new EventEmitter();
+  @Output() cancel = new EventEmitter();
+  
+  onConfirm() {
+    this.confirm.emit(true);
+    this.dialogRef.close(true);
+  }
+
+  onCancel() {
+    this.dialogRef.close(false);
+    this.cancel.emit();
+  }
 
 }
