648429edb87a6f28a1d4024f7055bfc1b2b62fc9
Harr-izle <harrietbuadee@gmail.com>
2024-11-08 16:45:26 +0000

    fix: creates a single input as a resuable component

---
+++ src/app/features/auth/components/input-field/input-field.component.ts
@@ -0,0 +1,63 @@
+import { CommonModule } from '@angular/common';
+import { Component, forwardRef, Input } from '@angular/core';
+import { ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-input-field',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './input-field.component.html',
+  styleUrl: './input-field.component.sass',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: forwardRef(() => InputFieldComponent),
+    },
+  ],
+})
+export class InputFieldComponent implements ControlValueAccessor{
+  @Input() type:string=''
+  @Input() label:string=''
+  @Input() placeholder:string=''
+  @Input() imgPath:string =''
+
+  isDisabled: boolean = false;
+  showPassword: boolean = false;
+  showConfirmPassword: boolean = false;
+
+
+  private _value: string = '';
+  get value(): string {
+    return this._value;
+  }
+  set value(val: string) {
+    this._value = val;
+    this.onChange(val);
+  }
+  onChange = (val: string) => {};
+  onTouched = () => {};
+  writeValue(value: string): void {
+    this.value = value;
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
+  }
+
+
+  togglePasswordVisibility(field: 'password' | 'confirmPassword') {
+    if (field === 'password') {
+      this.showPassword = !this.showPassword;
+    } else {
+      this.showConfirmPassword = !this.showConfirmPassword;
+    }
+  }
+
+
+}
