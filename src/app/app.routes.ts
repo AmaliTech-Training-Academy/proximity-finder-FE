1b3f022de07d10590e28c1b6457429ef44396c55
Harr-izle <harrietbuadee@gmail.com>
2024-11-08 09:15:17 +0000

    adds routes for reset password

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -21,5 +21,19 @@ export const routes: Routes = [
     {
         path:'login',
         loadComponent: () => import ('./features/auth/pages/login/login.component').then(m => m.LoginComponent)
-    }
+    },
+    {
+        path:'forgot-password',
+        loadComponent: () => import ('./features/auth/pages/forgotpassword-page/forgotpassword-page.component').then(m => m.ForgotpasswordPageComponent)
+    },
+    {
+        path:'confirmation',
+        loadComponent: () => import ('./features/auth/pages/email-verification/email-verification.component').then(m => m.EmailVerificationComponent)
+    },
+    {
+        path:'reset-password',
+        loadComponent: () => import ('./features/auth/pages/password-reset/password-reset.component').then(m => m.PasswordResetComponent)
+    },
+    
+        
 ];
