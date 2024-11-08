9a6150e248384dacd812da4b6f71f414bf2cd9d8
Harr-izle <harrietbuadee@gmail.com>
2024-11-08 17:16:27 +0000

    feat: designs role selection UI

--- src/app/features/auth/components/role-select/role-select.component.ts
+++ src/app/features/auth/components/role-select/role-select.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-role-select',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './role-select.component.html',
   styleUrl: './role-select.component.sass'
 })
