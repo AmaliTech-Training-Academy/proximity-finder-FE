5c2477c7499e1b1723dfb5c35600284c7a9d018a
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-11-08 14:05:32 +0000

    feat: adds and styles tab menu in requests page

---
+++ src/app/features/service-provider/pages/calls/calls.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calls',
+  standalone: true,
+  imports: [],
+  templateUrl: './calls.component.html',
+  styleUrl: './calls.component.sass'
+})
+export class CallsComponent {
+
+}
