65322c3ec6379aa19e16f04b495b34b4fc1e1f24
Harr-izle <harrietbuadee@gmail.com>
2024-11-10 10:54:08 +0000

    feat:creates components needed for provider registration

---
+++ src/app/features/pro-registration/components/service-experience/service-experience.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-service-experience',
+  standalone: true,
+  imports: [],
+  templateUrl: './service-experience.component.html',
+  styleUrl: './service-experience.component.sass'
+})
+export class ServiceExperienceComponent {
+
+}
---
+++ src/app/features/pro-registration/components/service-preference/service-preference.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-service-preference',
+  standalone: true,
+  imports: [],
+  templateUrl: './service-preference.component.html',
+  styleUrl: './service-preference.component.sass'
+})
+export class ServicePreferenceComponent {
+
+}
