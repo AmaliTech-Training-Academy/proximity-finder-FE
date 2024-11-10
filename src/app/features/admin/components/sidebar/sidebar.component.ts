058be01b76ccc5bec1021e0a4381266dfb965edb
IT Compliance <rhodaarthur48@gmail.com>
2024-11-10 16:51:24 +0000

    fix: replace svgs with mat-icon

--- src/app/features/admin/components/sidebar/sidebar.component.ts
+++ src/app/features/admin/components/sidebar/sidebar.component.ts
@@ -1,13 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, MatIconModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.sass'
 })
 export class SidebarComponent {
+  constructor() {
+    const iconRegistry = inject(MatIconRegistry);
+    const sanitizer = inject(DomSanitizer);
 
+    iconRegistry.addSvgIcon(
+      'dashboard',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/dashboard.svg')
+    );
+
+    iconRegistry.addSvgIcon(
+      'pro-account',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/pro-account.svg')
+    );
+
+    iconRegistry.addSvgIcon(
+      'users',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/user.svg')
+    );
+
+    iconRegistry.addSvgIcon(
+      'project',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/projects.svg')
+    );
+
+    iconRegistry.addSvgIcon(
+      'reports',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/clipboard.svg')
+    );
+
+    iconRegistry.addSvgIcon(
+      'reviews',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/reviews.svg')
+    );
+
+    iconRegistry.addSvgIcon(
+      'profile',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/profile.svg')
+    );
+
+    iconRegistry.addSvgIcon(
+      'help',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/help.svg')
+    );
+
+    iconRegistry.addSvgIcon(
+      'settings',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/settings.svg')
+    );
+
+    iconRegistry.addSvgIcon(
+      'logout',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/logout.svg')
+    );
+  }
 }
--- src/app/features/profile-management/pages/user-profile/user-profile.component.ts
+++ src/app/features/profile-management/pages/user-profile/user-profile.component.ts
@@ -9,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class UserProfileComponent {
   isFormActive = false
+  isAccountClicked = false
+
+  toggleAccountDetails() {
+    this.isAccountClicked = !this.isAccountClicked
+  }
 }
