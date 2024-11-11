c4ef6aa7e595db1079239c10b6337571a2e2469c
Harr-izle <harrietbuadee@gmail.com>
2024-11-11 11:26:45 +0000

    feat: styles basic info UI

--- src/app/features/pro-registration/components/basic-info/basic-info.component.ts
+++ src/app/features/pro-registration/components/basic-info/basic-info.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ProfileUpdateComponent } from '../profile-update/profile-update.component';
+import { FieldsComponent } from "../fields/fields.component";
 
 @Component({
   selector: 'app-basic-info',
   standalone: true,
-  imports: [],
+  imports: [ProfileUpdateComponent, FieldsComponent],
   templateUrl: './basic-info.component.html',
   styleUrl: './basic-info.component.sass'
 })
