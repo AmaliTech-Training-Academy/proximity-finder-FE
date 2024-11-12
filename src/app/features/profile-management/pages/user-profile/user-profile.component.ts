57eb9cbb3d6856c214b781020bc49082a494afb5
IT Compliance <rhodaarthur48@gmail.com>
2024-11-12 14:05:26 +0000

    fix: add form validation to user profile

--- src/app/features/profile-management/pages/user-profile/user-profile.component.ts
+++ src/app/features/profile-management/pages/user-profile/user-profile.component.ts
@@ -3,11 +3,12 @@ import { provideNativeDateAdapter } from '@angular/material/core';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { DeleteModalComponent } from '../../components/delete-modal/delete-modal.component';
 import { AccountDetailsComponent } from '../account-details/account-details.component';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [MatDialogModule],
+  imports: [MatDialogModule, ReactiveFormsModule],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.sass',
   providers: [provideNativeDateAdapter()]
@@ -18,6 +19,21 @@ export class UserProfileComponent {
   isAccountClicked = false
   isDeleteModal = false
 
+  constructor(private fb: FormBuilder) { }
+
+  userForm = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    email: ['', [Validators.required, Validators.email]],
+    phone: ['', [Validators.required,Validators.pattern(/^\d{10}$/)]]
+  })
+
+  accountInfoForm = this.fb.group({
+    bankName: ['', Validators.required],
+    accountName: ['', Validators.required],
+    accountAlias: [''],
+    accountNumber: ['', [Validators.required, Validators.pattern(/^\d{13}$/)]]
+  });
+  
   toggleAccountDetails() {
     this.isAccountClicked = !this.isAccountClicked
   }
