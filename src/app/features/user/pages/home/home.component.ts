bb1f8ecf1988c52e0eb87e320faa37c0e984cc27
IT Compliance <rhodaarthur48@gmail.com>
2024-11-14 14:53:34 +0000

    feat: add and complete trending services component layout and styling

--- src/app/features/user/pages/home/home.component.ts
+++ src/app/features/user/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { UserProfileHeaderComponent } from "../../../../shared/components/user-profile-header/user-profile-header.component";
 import { HeroComponent } from "../../components/hero/hero.component";
+import { TrendingServicesComponent } from "../../components/trending-services/trending-services.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [UserProfileHeaderComponent, HeroComponent],
+  imports: [UserProfileHeaderComponent, HeroComponent, TrendingServicesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.sass'
 })
