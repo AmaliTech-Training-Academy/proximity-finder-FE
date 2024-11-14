43f2a5470dd28573032d01ef6c976b8c10f78f2c
Harr-izle <harrietbuadee@gmail.com>
2024-11-14 16:00:49 +0000

    feat:validates payment option form

--- src/app/features/pro-registration/components/service-experience/service-experience.component.ts
+++ src/app/features/pro-registration/components/service-experience/service-experience.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { FieldsComponent } from "../fields/fields.component";
+import { ImageUploaderComponent } from "../../../service-provider/components/image-uploader/image-uploader.component";
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-service-experience',
   standalone: true,
-  imports: [FieldsComponent],
+  imports: [FieldsComponent, ImageUploaderComponent,ReactiveFormsModule,CommonModule],
   templateUrl: './service-experience.component.html',
   styleUrl: './service-experience.component.sass'
 })
 export class ServiceExperienceComponent {
+  experienceForm:FormGroup = this.fb.group({
+    title: ['', [Validators.required, Validators.minLength(3)]],
+    description: ['', [Validators.required, Validators.minLength(10)]]
+  });
+
+
+  constructor(private fb: FormBuilder) {}
+
+
+  onSubmit(): void {
+    if (this.experienceForm.valid) {
+      
+    } 
+  }
 
 }
