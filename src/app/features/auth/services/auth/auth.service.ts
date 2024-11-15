9ba864fd04d1e06e5b6491c3390869e98ed0c165
Harr-izle <harrietbuadee@gmail.com>
2024-11-15 09:03:42 +0000

    feat:creates services for signup and login

---
+++ src/app/features/auth/services/auth/auth.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+apiUrl:string ='http://16.170.246.127:8080/'
+
+  constructor() { }
+}
