92ccd6febd3f3873a182b28bf9f862a03f889acc
IT Compliance <rhodaarthur48@gmail.com>
2024-11-15 13:07:58 +0000

    feat: complete faq component layout and styling

--- src/app/features/user/pages/home/home.component.ts
+++ src/app/features/user/pages/home/home.component.ts
@@ -5,11 +5,13 @@ import { TrendingServicesComponent } from "../../components/trending-services/tr
 import { CoreFeaturesComponent } from "../../components/core-features/core-features.component";
 import { DiscoverComponent } from "../../components/discover/discover.component";
 import { FeedbackComponent } from "../../components/feedback/feedback.component";
+import { FaqComponent } from "../../components/faq/faq.component";
+import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [UserProfileHeaderComponent, HeroComponent, TrendingServicesComponent, CoreFeaturesComponent, DiscoverComponent, FeedbackComponent],
+  imports: [UserProfileHeaderComponent, HeroComponent, TrendingServicesComponent, CoreFeaturesComponent, DiscoverComponent, FeedbackComponent, FaqComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.sass'
 })
