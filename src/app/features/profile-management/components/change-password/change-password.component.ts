f09444d99af42adef161fd87fa8561416fa3f1ac
IT Compliance <rhodaarthur48@gmail.com>
2024-11-16 09:29:06 +0000

    fix: add password input component with validation and error handling

--- src/app/features/profile-management/components/change-password/change-password.component.ts
+++ src/app/features/profile-management/components/change-password/change-password.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { InputFieldComponent } from "../../../auth/components/input-field/input-field.component";
+import { PasswordInputComponent } from "../password-input/password-input.component";
+import { FormValidators, passwordValidator } from '../../../../utils/passwordValidator';
 
 @Component({
   selector: 'app-change-password',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, PasswordInputComponent],
   templateUrl: './change-password.component.html',
   styleUrl: './change-password.component.sass'
 })
@@ -14,33 +15,9 @@ export class ChangePasswordComponent{
     oldPassword: ['', [Validators.required, Validators.minLength(12)]],
     newPassword: ['', [Validators.required, Validators.minLength(12)]],
     confirmPassword: ['', Validators.required]
-  }, {validators: [this.passwordMatchValidator, this.passwordMismatchValidator]})
-
-  oldPasswordVisible = false;
-  newPasswordVisible = false;
-  confirmPasswordVisible = false;
+  }, {validators: [FormValidators.passwordMatchValidator, FormValidators.passwordMismatchValidator, passwordValidator]})
 
   constructor (private fb: FormBuilder) {}
-
-  passwordMatchValidator(form: FormGroup): {[key: string]: boolean} | null {
-    return form.get('newPassword')?.value === form.get('confirmPassword')?.value ? null : {'mismatch': true}
-  }
-
-  passwordMismatchValidator(form: FormGroup): {[key: string]: boolean} | null {
-    return form.get('oldPassword')?.value === form.get('newPassword')?.value ? {'mismatch': true} : null
-  }
-
-  togglePasswordVisibility(type: string): void {
-    if (type === 'oldPassword') {
-      this.oldPasswordVisible = !this.oldPasswordVisible
-    }
-    else if (type === 'newPassword') {
-      this.newPasswordVisible = !this.newPasswordVisible
-    }
-    else {
-      this.confirmPasswordVisible = !this.confirmPasswordVisible
-    }
-  }
 
   onSubmit() {
     if (this.changePasswordForm.valid) {
