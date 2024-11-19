b47e1ff8bc086789f0ec2031c7034b71905a87f9
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-11-19 10:06:52 +0000

    feat: adds service provider basic info management

---
+++ src/app/features/service-provider/components/pro-basic-info/pro-basic-info.component.ts
@@ -0,0 +1,65 @@
+import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-pro-basic-info',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './pro-basic-info.component.html',
+  styleUrl: './pro-basic-info.component.sass',
+})
+export class ProBasicInfoComponent {
+  isEditing = false;
+
+  basicInfoForm: FormGroup = this.fb.group({
+    businessName: [
+      { value: 'Strive Studios', disabled: !this.isEditing },
+      Validators.required,
+    ],
+    businessEmail: [
+      { value: 'oliviarhye@gmail.com', disabled: !this.isEditing },
+      Validators.required,
+    ],
+    businessAddress: [
+      { value: 'AG-345-4738', disabled: !this.isEditing },
+      Validators.required,
+    ],
+    businessOwnerName: [
+      { value: 'Sarah Johnson', disabled: !this.isEditing },
+      Validators.required,
+    ],
+    phoneNumber: [
+      { value: '020 378 234 3672', disabled: !this.isEditing },
+      Validators.required,
+    ],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  setEditing() {
+    this.isEditing = true;
+    this.toggleFormControls();
+    console.log(this.isEditing);
+  }
+
+  cancelEditing() {
+    this.isEditing = false;
+    this.toggleFormControls();
+  }
+
+  toggleFormControls() {
+    Object.keys(this.basicInfoForm.controls).forEach((controlName) => {
+      const control = this.basicInfoForm.get(controlName);
+      if (this.isEditing) {
+        control?.enable();
+      } else {
+        control?.disable();
+      }
+    });
+  }
+}
--- src/app/features/service-provider/pages/settings/settings.component.ts
+++ src/app/features/service-provider/pages/settings/settings.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { TabViewModule } from 'primeng/tabview';
+import { ProBasicInfoComponent } from '../../components/pro-basic-info/pro-basic-info.component';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [],
+  imports: [TabViewModule, ProBasicInfoComponent],
   templateUrl: './settings.component.html',
-  styleUrl: './settings.component.sass'
+  styleUrl: './settings.component.sass',
 })
-export class SettingsComponent {
-
-}
+export class SettingsComponent {}
