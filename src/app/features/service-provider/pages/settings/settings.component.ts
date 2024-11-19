b7865bebca9f19f97cb287b066ebf7db0cea2d26
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-11-19 10:58:04 +0000

    feat: styles service provider's business profile

--- src/app/features/service-provider/pages/settings/settings.component.ts
+++ src/app/features/service-provider/pages/settings/settings.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { TabViewModule } from 'primeng/tabview';
 import { ProBasicInfoComponent } from '../../components/pro-basic-info/pro-basic-info.component';
+import { ProAboutBusinessComponent } from '../../components/pro-about-business/pro-about-business.component';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [TabViewModule, ProBasicInfoComponent],
+  imports: [TabViewModule, ProBasicInfoComponent, ProAboutBusinessComponent],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.sass',
 })
