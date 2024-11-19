77fbe63340940c2ea7d9c7fa81d8e8d492d42e30
Harr-izle <harrietbuadee@gmail.com>
2024-11-19 15:39:27 +0000

    feat:implememts user login

--- src/app/features/auth/components/login-input/login-input.component.ts
+++ src/app/features/auth/components/login-input/login-input.component.ts
@@ -1,36 +1,61 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { InputFieldComponent } from '../input-field/input-field.component';
 import { AuthService } from '../../services/auth/auth.service';
-
+import { Router } from '@angular/router';
+import { Notyf } from 'notyf';
+import { NOTYF } from '../../../../shared/notify/notyf.token';
 
 @Component({
   selector: 'app-login-input',
   standalone: true,
-  imports: [CommonModule,ReactiveFormsModule,RouterLink,InputFieldComponent],
+  imports: [CommonModule, ReactiveFormsModule, RouterLink, InputFieldComponent],
   templateUrl: './login-input.component.html',
-  styleUrl: './login-input.component.sass'
+  styleUrls: ['./login-input.component.sass']
 })
 export class LoginInputComponent {
   loginForm: FormGroup = this.formBuilder.group({
-    email: ['',[ Validators.required, Validators.email]],
-    password: ['',Validators.required]
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required]
   });
 
-  constructor(private formBuilder:FormBuilder,private authService:AuthService) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+    @Inject(NOTYF) private notyf: Notyf
+  ) {}
 
   onSubmit() {
     if (this.loginForm.valid) {
-      const data = this.loginForm.value; 
-      this.authService.login(data).subscribe((res) => {
-        console.log('Login successful:', res);
-      }, (error) => {
-        console.error('Login failed:', error);
+      const { email, password } = this.loginForm.value; 
+      this.authService.login(email, password).subscribe({
+        next: (res) => {
+          switch(res.role) {
+            case 'ADMIN':
+              this.notyf.success('Login Successful');
+              this.router.navigateByUrl('/admin/dashboard');
+              break;
+            case 'SEEKER':
+              this.notyf.success('Login Successful');
+              this.router.navigateByUrl('');
+              break;
+            case 'PROVIDER':
+              this.notyf.success('Login Successful');
+              this.router.navigateByUrl('provider/dashboard');
+              break;
+            default:
+              this.router.navigateByUrl('');
+          }
+          this.loginForm.reset();
+        },
+        error: (error) => {
+          this.notyf.error('Login failed. Please check your credentials.');
+          
+        }
       });
     }
   }
-  
-
 }
