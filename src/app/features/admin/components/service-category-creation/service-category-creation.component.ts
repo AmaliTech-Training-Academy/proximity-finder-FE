7e18219da75466ec896a0d0db1a3651d97258b89
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-11-20 11:43:47 +0000

    fix: moves service category tab to different component

--- src/app/features/admin/components/service-category-creation/service-category-creation.component.ts
+++ src/app/features/admin/components/service-category-creation/service-category-creation.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { ServiceListingComponent } from '../service-listing/service-listing.component';
 
 @Component({
   selector: 'app-service-category-creation',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule, ServiceListingComponent],
   templateUrl: './service-category-creation.component.html',
-  styleUrl: './service-category-creation.component.sass'
+  styleUrl: './service-category-creation.component.sass',
 })
-export class ServiceCategoryCreationComponent {
-
-}
+export class ServiceCategoryCreationComponent {}
--- src/app/features/admin/pages/admin-settings/admin-settings.component.ts
+++ src/app/features/admin/pages/admin-settings/admin-settings.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { TabViewModule } from 'primeng/tabview';
-import { ButtonModule } from 'primeng/button';
 import { ServiceListingComponent } from '../../components/service-listing/service-listing.component';
+import { ServiceCategoryCreationComponent } from '../../components/service-category-creation/service-category-creation.component';
 
 @Component({
   selector: 'app-admin-settings',
   standalone: true,
-  imports: [TabViewModule, ButtonModule, ServiceListingComponent],
+  imports: [TabViewModule, ServiceCategoryCreationComponent],
   templateUrl: './admin-settings.component.html',
   styleUrl: './admin-settings.component.sass',
 })
