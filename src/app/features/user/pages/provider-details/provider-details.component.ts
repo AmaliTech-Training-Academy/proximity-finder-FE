dad3c45306dcfa28189f088dadbaaeebb4f92794
IT Compliance <rhodaarthur48@gmail.com>
2024-11-23 20:42:07 +0000

    feat: add featured projects component

--- src/app/features/user/pages/provider-details/provider-details.component.ts
+++ src/app/features/user/pages/provider-details/provider-details.component.ts
@@ -4,11 +4,13 @@ import { HeaderComponent } from "../../../service-provider/components/header/hea
 import { UserProfileHeaderComponent } from "../../../../shared/components/user-profile-header/user-profile-header.component";
 import { ProfileDetailsComponent } from "../../provider-details/components/profile-details/profile-details.component";
 import { ScheduleOverviewComponent } from "../../provider-details/components/schedule-overview/schedule-overview.component";
+import { FeaturedProjectsComponent } from "../../provider-details/components/featured-projects/featured-projects.component";
+import { ReviewsComponent } from "../../provider-details/components/reviews/reviews.component";
 
 @Component({
   selector: 'app-provider-details',
   standalone: true,
-  imports: [NavbarComponent, UserProfileHeaderComponent, ProfileDetailsComponent, ScheduleOverviewComponent],
+  imports: [NavbarComponent, UserProfileHeaderComponent, ProfileDetailsComponent, ScheduleOverviewComponent, FeaturedProjectsComponent, ReviewsComponent],
   templateUrl: './provider-details.component.html',
   styleUrl: './provider-details.component.sass'
 })
