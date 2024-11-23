faf9ee6559daf458a6551ab4a0ec5718cd8a8e35
IT Compliance <rhodaarthur48@gmail.com>
2024-11-23 19:44:49 +0000

    feat: add schedule overview component

--- src/app/features/user/pages/provider-details/provider-details.component.ts
+++ src/app/features/user/pages/provider-details/provider-details.component.ts
@@ -3,11 +3,12 @@ import { NavbarComponent } from "../../components/navbar/navbar.component";
 import { HeaderComponent } from "../../../service-provider/components/header/header.component";
 import { UserProfileHeaderComponent } from "../../../../shared/components/user-profile-header/user-profile-header.component";
 import { ProfileDetailsComponent } from "../../provider-details/components/profile-details/profile-details.component";
+import { ScheduleOverviewComponent } from "../../provider-details/components/schedule-overview/schedule-overview.component";
 
 @Component({
   selector: 'app-provider-details',
   standalone: true,
-  imports: [NavbarComponent, UserProfileHeaderComponent, ProfileDetailsComponent],
+  imports: [NavbarComponent, UserProfileHeaderComponent, ProfileDetailsComponent, ScheduleOverviewComponent],
   templateUrl: './provider-details.component.html',
   styleUrl: './provider-details.component.sass'
 })
