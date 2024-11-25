92e19c31826d59db8dd7cc1a184e6696f3e0bea2
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-11-25 14:38:30 +0000

    feat: Implements sending service preference data to backend

--- src/app/utils/endPoints.ts
+++ src/app/utils/endPoints.ts
@@ -1,12 +1,10 @@
-
 export const excludedEndpoints = [
-    '/auth/public/',
-    '/password/reset-request',
-    '/password/reset-password',
-    '/api/v1/services',
-    '/auth/info',
-    '/auth/update/info',
-    '/auth/public/update-profile-picture'
-
-  ];
-  
+  '/auth/public/',
+  '/password/reset-request',
+  '/password/reset-password',
+  '/api/v1/services',
+  '/auth/info',
+  '/auth/update/info',
+  '/auth/public/update-profile-picture',
+  '/api/v1/provider-services',
+];
