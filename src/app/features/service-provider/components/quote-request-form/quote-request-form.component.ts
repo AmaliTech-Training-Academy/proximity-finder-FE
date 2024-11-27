a2f85d3de0f29cbd78ee3dd705e0fbeab783448c
Harr-izle <harrietbuadee@gmail.com>
2024-11-27 12:55:35 +0000

    feat:creates component for quote request form

---
+++ src/app/features/service-provider/components/quote-request-form/quote-request-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quote-request-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './quote-request-form.component.html',
+  styleUrl: './quote-request-form.component.sass'
+})
+export class QuoteRequestFormComponent {
+
+}
