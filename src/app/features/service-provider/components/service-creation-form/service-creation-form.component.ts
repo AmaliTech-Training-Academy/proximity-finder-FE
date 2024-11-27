6f05f1e2c4ba0fa1cf320579af8884a54cf439ad
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-11-27 09:25:01 +0000

    fix: adjusts ui for service provider service creation form

--- src/app/features/service-provider/components/service-creation-form/service-creation-form.component.ts
+++ src/app/features/service-provider/components/service-creation-form/service-creation-form.component.ts
@@ -4,8 +4,9 @@ import {
   FormBuilder,
   FormGroup,
   Validators,
+  FormArray,
 } from '@angular/forms';
-import { accountPreferences } from '../../data';
+import { accountPreferences, bookingDays } from '../../data';
 import { DropdownModule } from 'primeng/dropdown';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
@@ -13,6 +14,8 @@ import { InputTextareaModule } from 'primeng/inputtextarea';
 import { CommonModule } from '@angular/common';
 import { FileUploaderComponent } from '../file-uploader/file-uploader.component';
 import { ImageUploaderComponent } from '../image-uploader/image-uploader.component';
+import { ServiceService } from '../../../../core/services/service.service';
+import { ITime } from '../../../../core/models/ITime';
 
 @Component({
   selector: 'app-service-creation-form',
@@ -31,16 +34,67 @@ import { ImageUploaderComponent } from '../image-uploader/image-uploader.compone
   styleUrl: './service-creation-form.component.sass',
 })
 export class ServiceCreationFormComponent {
+  serviceCategories$ = this.serviceService.serviceCategories$;
   accountPreferences = accountPreferences;
+  days = bookingDays;
+  timeOptions: ITime[] = [];
 
   serviceForm: FormGroup = this.fb.group({
     jobTitle: ['', Validators.required],
     accountPreference: ['', Validators.required],
+    bookingDays: this.fb.array([this.createBookingDay()]),
     businessCertificate: [null],
     serviceDescription: ['', Validators.required],
     schedulingPolicy: [''],
     projectPictures: [[]],
   });
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private serviceService: ServiceService
+  ) {}
+
+  generateTimeOptions(step: number) {
+    const times: ITime[] = [];
+    for (let hour = 0; hour < 24; hour++) {
+      for (let minute = 0; minute < 60; minute += step) {
+        const rawTime = new Date();
+        rawTime.setHours(hour, minute, 0);
+
+        const options = {
+          hour: 'numeric',
+          minute: 'numeric',
+          hour12: true,
+        } as Intl.DateTimeFormatOptions;
+        const formattedTime = new Intl.DateTimeFormat('en-US', options).format(
+          rawTime
+        );
+
+        times.push({ name: formattedTime, value: formattedTime });
+      }
+    }
+    this.timeOptions = times;
+  }
+
+  get bookingDaysFormArray(): FormArray {
+    return this.serviceForm.get('bookingDays') as FormArray;
+  }
+
+  createBookingDay(): FormGroup {
+    return this.fb.group({
+      dayOfWeek: ['', Validators.required],
+      startTime: ['', Validators.required],
+      endTime: ['', Validators.required],
+    });
+  }
+
+  addBookingDay() {
+    this.bookingDaysFormArray.push(this.createBookingDay());
+  }
+
+  removeBookingDay(index: number) {
+    if (this.bookingDaysFormArray.length > 1) {
+      this.bookingDaysFormArray.removeAt(index);
+    }
+  }
 }
