7741cb62d407d0e1627f85647ce299e10be0ee0a
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-11-28 11:34:24 +0000

    styles main chat view

--- src/app/features/chat/pages/chat/chat.component.ts
+++ src/app/features/chat/pages/chat/chat.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../../service-provider/components/header/header.component';
-import { RouterOutlet } from '@angular/router';
 import { ChatListComponent } from '../../components/chat-list/chat-list.component';
+import { ChatDetailComponent } from '../../components/chat-detail/chat-detail.component';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, ChatListComponent],
+  imports: [HeaderComponent, ChatListComponent, ChatDetailComponent],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.sass',
 })
