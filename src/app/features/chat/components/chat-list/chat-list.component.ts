613b2e70ce70a425cd9bfdf6580d8d567a49ce9e
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-11-28 10:39:31 +0000

    feat: styles chat list sidebar

--- src/app/features/chat/components/chat-list/chat-list.component.ts
+++ src/app/features/chat/components/chat-list/chat-list.component.ts
@@ -3,11 +3,19 @@ import { InputIconModule } from 'primeng/inputicon';
 import { IconFieldModule } from 'primeng/iconfield';
 import { InputTextModule } from 'primeng/inputtext';
 import { FormsModule } from '@angular/forms';
+import { ChatComponent } from '../../pages/chat/chat.component';
+import { ChatItemComponent } from '../chat-item/chat-item.component';
 
 @Component({
   selector: 'app-chat-list',
   standalone: true,
-  imports: [InputIconModule, IconFieldModule, InputTextModule, FormsModule],
+  imports: [
+    InputIconModule,
+    IconFieldModule,
+    InputTextModule,
+    FormsModule,
+    ChatItemComponent,
+  ],
   templateUrl: './chat-list.component.html',
   styleUrl: './chat-list.component.sass',
 })
