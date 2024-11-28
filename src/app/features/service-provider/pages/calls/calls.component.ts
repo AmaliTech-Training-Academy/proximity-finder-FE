79fb987fb7d2b2952cccd47677ae016e6efcd633
Harr-izle <harrietbuadee@gmail.com>
2024-11-28 10:19:03 +0000

    feat:designs call request page

--- src/app/features/service-provider/pages/calls/calls.component.ts
+++ src/app/features/service-provider/pages/calls/calls.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
+import { TableModule } from 'primeng/table';
+import { calls } from '../../data';
 
 @Component({
   selector: 'app-calls',
   standalone: true,
-  imports: [],
+  imports: [TableModule, CommonModule,],
   templateUrl: './calls.component.html',
   styleUrl: './calls.component.sass'
 })
 export class CallsComponent {
+  calls = calls
+
+  statuses = ['Pending', 'Declined', 'Scheduled']; 
+  activeQuote: any = null; 
+
+  toggleMenu(quote: any) {
+    this.activeQuote = this.activeQuote === quote ? null : quote;
+  }
+
+
+  updateStatus(quote: any, status: string) {
+    quote.status = status;
+    this.activeQuote = null; 
+  }
+  @HostListener('document:click', ['$event'])
+  closeMenu(event: Event) {
+    this.activeQuote = null;
+  }
 
 }
