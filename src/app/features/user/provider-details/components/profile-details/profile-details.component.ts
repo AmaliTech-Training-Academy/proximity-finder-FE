2a2b51b4f0bc36431371aebf990ca7d05c053b24
IT Compliance <rhodaarthur48@gmail.com>
2024-11-30 20:14:14 +0000

    feat: implement session storage for provider data

--- src/app/features/user/provider-details/components/profile-details/profile-details.component.ts
+++ src/app/features/user/provider-details/components/profile-details/profile-details.component.ts
@@ -1,14 +1,14 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { FormGroup,FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RatingModule } from 'primeng/rating';
 import { AvailabilityFormComponent } from "../availability-form/availability-form.component";
 import { DialogModule } from 'primeng/dialog';
 import { ImageUploaderComponent } from '../../../../service-provider/components/image-uploader/image-uploader.component';
 import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';
 import { ProDetails } from '../../../../service-discovery/models/pro-details';
 import { NOTYF } from '../../../../../shared/notify/notyf.token';
 import { ProviderDataService } from '../../../../service-discovery/services/provider-data.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-profile-details',
@@ -17,10 +17,11 @@ import { ProviderDataService } from '../../../../service-discovery/services/prov
   templateUrl: './profile-details.component.html',
   styleUrl: './profile-details.component.sass'
 })
-export class ProfileDetailsComponent {
+export class ProfileDetailsComponent implements OnInit, OnDestroy{
   value: number = 4;
   provider!: ProDetails;
   private notyf = inject(NOTYF)
+  providerSubscription: Subscription | null = null
 
   visible: boolean = false;
   modals = {
@@ -56,13 +57,23 @@ export class ProfileDetailsComponent {
 
 
   ngOnInit() {
-    this.providerService.selectedProvider$.subscribe((provider) => {
-      if (provider) {
-        this.provider = provider;
-      } else {
-        this.notyf.error('Provider not found');
-      }
-    })
+    const storedProvider = this.providerService.getSelectedProvider();
+  
+    if (storedProvider) {
+      this.provider = storedProvider;
+    } else {
+      this.providerSubscription = this.providerService.selectedProvider$.subscribe((provider) => {
+        if (provider) {
+          this.provider = provider;
+        } else {
+          this.notyf.error('Provider not found');
+        }
+      });
+    }
   }
 
+
+  ngOnDestroy(): void {
+    this.providerSubscription?.unsubscribe();
+  }
 }
