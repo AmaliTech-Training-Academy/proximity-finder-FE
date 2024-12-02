bb6827ac54da28bbafbe6eeb18ec9a5ac052befb
Harr-izle <harrietbuadee@gmail.com>
2024-12-02 13:27:38 +0000

    feat:updates urls

--- src/app/features/user/provider-details/components/profile-details/profile-details.component.ts
+++ src/app/features/user/provider-details/components/profile-details/profile-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, Inject, inject } from '@angular/core';
 import { FormGroup,FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RatingModule } from 'primeng/rating';
 import { AvailabilityFormComponent } from "../availability-form/availability-form.component";
@@ -9,6 +9,12 @@ import { Router } from '@angular/router';
 import { ProDetails } from '../../../../service-discovery/models/pro-details';
 import { NOTYF } from '../../../../../shared/notify/notyf.token';
 import { ProviderDataService } from '../../../../service-discovery/services/provider-data.service';
+import { Quote } from '../../../../service-provider/models/quoteData';
+import { QuoteService } from '../../../../service-provider/services/quote/quote.service';
+import { CallService } from '../../../../service-provider/services/call/call.service';
+import { callData } from '../../../../service-provider/models/callData';
+
+
 
 @Component({
   selector: 'app-profile-details',
@@ -33,7 +39,7 @@ export class ProfileDetailsComponent {
   })
 
   quoteForm:FormGroup = this.formBuilder.group({
-  name: ["",Validators.required],
+  title: ["",Validators.required],
    location: ["",Validators.required],
     date: ["",Validators.required],
     time: ["",Validators.required],
@@ -44,15 +50,8 @@ export class ProfileDetailsComponent {
     
   })
 
-  constructor(private formBuilder:FormBuilder, private providerService: ProviderDataService){}
+  constructor(private formBuilder:FormBuilder, private providerService: ProviderDataService,private quoteService:QuoteService, private callService:CallService){}
 
-  
-  showDialog(type: 'quote' | 'call') {
-    this.modals[type] = true;
-  }
-  closeDialog(type: 'quote' | 'call') {
-    this.modals[type] = false;
-  }
 
 
   ngOnInit() {
@@ -65,4 +64,93 @@ export class ProfileDetailsComponent {
     })
   }
 
+  showDialog(type: 'quote' | 'call') {
+    this.modals[type] = true;
+  }
+  closeDialog(type: 'quote' | 'call') {
+    this.modals[type] = false;
+  }
+
+  onImagesChange(event: Event) {
+    const input = event.target as HTMLInputElement; HTMLInputElement
+    if (input?.files) {
+      const fileList = input.files; 
+      const files: File[] = Array.from(fileList);
+      this.quoteForm.patchValue({ images: files });
+    }
+  }
+  
+  
+onQuoteSubmit() {
+  if (this.quoteForm.valid) {
+    const { title, location, date, time, endDate, endTime, description, info, images } = this.quoteForm.value;
+
+    
+    const selectedProvider = this.providerService.getSelectedProvider();
+
+    const email = selectedProvider ? selectedProvider.email : null;
+
+    const formData = new FormData();
+    formData.append("title", title);
+    formData.append("location", location);
+    formData.append("startDate", date);
+    formData.append("startTime", time);
+    formData.append("endDate", endDate);
+    formData.append("endTime", endTime);
+    formData.append("description", description);
+    formData.append("info", info || "");
+    
+    if (email) {
+      formData.append("email", email);
+    }
+
+    if (images) {
+      for (const image of images) {
+        formData.append("images", image);
+      }
+    }
+
+    this.quoteService.createQuote(formData).subscribe({
+      next: (quote: Quote) => {
+        this.notyf.success("Quote Sent Successfully");
+        this.modals.quote = false;
+      },
+      error: (error) => {
+        this.notyf.error("Failed to send quote");
+      },
+    });
+  }
+}
+
+
+  onCallRequest() {
+    const { phoneNumber } = this.callForm.value;
+  
+
+    const selectedProvider = this.providerService.getSelectedProvider();
+  
+    if (selectedProvider) {
+      const { email } = selectedProvider; 
+  
+      const data: callData = {
+        phoneNumber,
+        email, 
+      };
+  
+      this.callService.sendCallRequest(data).subscribe({
+       
+        next: (response) => {
+          console.log(response)
+          this.notyf.success("Call Request Sent Successfully");
+          this.modals.call = false;
+        },
+        error: (error) => {
+          console.log(error)
+          this.notyf.error("Failed to send call request");
+        },
+      });
+    } else {
+      this.notyf.error("No provider selected");
+    }
+  }
 }
