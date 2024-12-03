2b5c86092b5d5a915230b760f5efbabe6d63adf5
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-12-03 13:51:49 +0000

    Styles linked accounts list

--- src/app/features/service-provider/components/pro-account-info/pro-account-info.component.ts
+++ src/app/features/service-provider/components/pro-account-info/pro-account-info.component.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { MenuModule } from 'primeng/menu';
 
 @Component({
   selector: 'app-pro-account-info',
   standalone: true,
-  imports: [ButtonModule],
+  imports: [ButtonModule, MenuModule],
   templateUrl: './pro-account-info.component.html',
   styleUrl: './pro-account-info.component.sass',
 })
-export class ProAccountInfoComponent {}
+export class ProAccountInfoComponent {
+  items = [
+    {
+      label: 'Edit',
+      icon: 'pi pi-pencil',
+    },
+    {
+      label: 'Delete',
+      icon: 'pi pi-trash',
+    },
+  ];
+
+  linkedAccounts = [
+    {
+      name: 'American Express',
+    },
+    {
+      name: 'Fidelity Bank - EUR',
+    },
+    {
+      name: 'MTN Mobile Money',
+    },
+    {
+      name: 'Stanbic Bank',
+    },
+  ];
+}
