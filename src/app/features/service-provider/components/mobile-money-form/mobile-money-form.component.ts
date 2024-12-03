eeba49ee87164b9d15b6f03d1798697cc5e5d390
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-12-03 15:12:18 +0000

    Styles mobile money panel form

--- src/app/features/service-provider/components/mobile-money-form/mobile-money-form.component.ts
+++ src/app/features/service-provider/components/mobile-money-form/mobile-money-form.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-mobile-money-form',
   standalone: true,
-  imports: [],
+  imports: [DropdownModule, InputTextModule, ButtonModule],
   templateUrl: './mobile-money-form.component.html',
-  styleUrl: './mobile-money-form.component.sass'
+  styleUrl: './mobile-money-form.component.sass',
 })
 export class MobileMoneyFormComponent {
+  @Output() closeModalEvent = new EventEmitter<boolean>();
 
+  closeDialog() {
+    this.closeModalEvent.emit(false);
+  }
 }
