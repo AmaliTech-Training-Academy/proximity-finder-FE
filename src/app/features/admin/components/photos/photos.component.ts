5569b03229cad15bfcbfab475adc19121fee6ac7
IT Compliance <rhodaarthur48@gmail.com>
2024-12-04 11:59:19 +0000

    feat: add photo service and implement photo gallery with reviews in admin pro details

--- src/app/features/admin/components/photos/photos.component.ts
+++ src/app/features/admin/components/photos/photos.component.ts
@@ -1,22 +1,50 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { GalleriaModule } from 'primeng/galleria';
+import { PhotoService } from '../../../../core/services/photo.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-photos',
   standalone: true,
-  imports: [],
+  imports: [GalleriaModule, CommonModule, FormsModule],
   templateUrl: './photos.component.html',
-  styleUrl: './photos.component.sass'
+  styleUrl: './photos.component.sass',
 })
 export class PhotosComponent {
-  images: string[] = [
-    'assets/images/core.png',
-    'assets/images/occupation.png',
-    'assets/images/occupation.png',
-    'assets/images/occupation.png',
-    'assets/images/occupation.png',
-    'assets/images/occupation.png',
-    'assets/images/cover-image.png',
-    'assets/images/background.jpg',
-    'assets/images/Hero.png'
-  ];
+  displayCustom!: boolean;
+
+    activeIndex: number = 0;
+
+    images: any[] | undefined;
+
+    responsiveOptions: any[] = [
+        {
+            breakpoint: '1500px',
+            numVisible: 5
+        },
+        {
+            breakpoint: '1024px',
+            numVisible: 3
+        },
+        {
+            breakpoint: '768px',
+            numVisible: 2
+        },
+        {
+            breakpoint: '560px',
+            numVisible: 1
+        }
+    ];
+
+    constructor(private photoService: PhotoService) {}
+
+    ngOnInit() {
+        this.photoService.getImages().then((images) => (this.images = images));
+    }
+
+    imageClick(index: number) {
+        this.activeIndex = index;
+        this.displayCustom = true;
+    }
 }
--- src/app/features/admin/pages/admin-pro-details/admin-pro-details.component.ts
+++ src/app/features/admin/pages/admin-pro-details/admin-pro-details.component.ts
@@ -7,12 +7,13 @@ import { AboutComponent } from "../../components/about/about.component";
 import { PhotosComponent } from "../../components/photos/photos.component";
 import { CommonModule } from '@angular/common';
 import { AdminCredentialsComponent } from "../../components/admin-credentials/admin-credentials.component";
+import { ProDetailsReviewComponent } from "../../components/pro-details-review/pro-details-review.component";
 
 @Component({
   selector: 'app-admin-pro-details',
   standalone: true,
   imports: [RatingModule, FormsModule, TabMenuModule, TabViewModule, AboutComponent,
-     PhotosComponent, CommonModule, AdminCredentialsComponent],
+    PhotosComponent, CommonModule, AdminCredentialsComponent, ProDetailsReviewComponent],
   templateUrl: './admin-pro-details.component.html',
   styleUrl: './admin-pro-details.component.sass'
 })
