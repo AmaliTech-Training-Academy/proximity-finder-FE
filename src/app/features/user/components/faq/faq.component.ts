1c718d4701576763f3094ebd7ea8788933137802
IT Compliance <rhodaarthur48@gmail.com>
2024-12-09 11:08:16 +0000

    Implement dynamic FAQ loading and improve component structure

--- src/app/features/user/components/faq/faq.component.ts
+++ src/app/features/user/components/faq/faq.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AccordionModule } from 'primeng/accordion';
+import { HelpAndSupportService } from '../../../help-and-support/services/help-and-support.service';
+import { Faq } from '../../../help-and-support/models/faqs';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-faq',
   standalone: true,
-  imports: [AccordionModule],
+  imports: [AccordionModule, CommonModule],
   templateUrl: './faq.component.html',
   styleUrl: './faq.component.sass'
 })
-export class FaqComponent {
+export class FaqComponent implements OnInit{
+  faqs!: Observable<Faq[]>
 
+  constructor (private faqSerivce: HelpAndSupportService) {}
+
+  ngOnInit() {
+    this.faqs = this.faqSerivce.getAllFaqs()
+  }
 }
