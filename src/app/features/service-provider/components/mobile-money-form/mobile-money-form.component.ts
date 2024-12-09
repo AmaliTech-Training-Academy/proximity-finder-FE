14055b20700617c2b25af40f93acb2965be4f178
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-12-09 09:23:28 +0000

    Fetches mobile money providers

--- src/app/features/service-provider/components/mobile-money-form/mobile-money-form.component.ts
+++ src/app/features/service-provider/components/mobile-money-form/mobile-money-form.component.ts
@@ -1,7 +1,8 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { DropdownModule } from 'primeng/dropdown';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
+import { UserMobileMoneyService } from '../../../profile-management/services/user-mobile-money.service';
 
 @Component({
   selector: 'app-mobile-money-form',
@@ -10,8 +11,19 @@ import { ButtonModule } from 'primeng/button';
   templateUrl: './mobile-money-form.component.html',
   styleUrl: './mobile-money-form.component.sass',
 })
-export class MobileMoneyFormComponent {
+export class MobileMoneyFormComponent implements OnInit {
+  mobileMoneyProviders: string[] = [];
   @Output() closeModalEvent = new EventEmitter<boolean>();
+
+  constructor(private mobileMoneyService: UserMobileMoneyService) {}
+
+  ngOnInit() {
+    this.mobileMoneyService.getServiceProviders().subscribe({
+      next: (providers) => (this.mobileMoneyProviders = providers),
+      error: (error) =>
+        console.error('Failed to fetch mobile money service providers', error),
+    });
+  }
 
   closeDialog() {
     this.closeModalEvent.emit(false);
