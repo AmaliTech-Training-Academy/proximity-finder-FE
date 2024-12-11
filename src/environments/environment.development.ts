84f845701bc0115ccee019129902fecc4ae4708f
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-12-11 16:31:22 +0000

    fix: adds missing environment variables

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,4 @@
 export const environment = {
-
   production: false,
   baseUrl: 'http://54.91.220.36:8080/api',
   registration:
@@ -7,5 +6,6 @@ export const environment = {
   searchUrl: 'http://3.136.48.244:8080/api/v1',
   faq: 'https://api.proximity-finder.amalitech-dev.net/api/v1/support/',
   geminiAiKey: 'AIzaSyCM8_y75mvwbhOZP_jW7ZW1-R0Ug4W8V20',
-
+  quote:
+    'https://api.proximity-finder.amalitech-dev.net/api/v1/quote-service/quotes',
 };
