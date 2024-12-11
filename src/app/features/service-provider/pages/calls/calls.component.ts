67400ae47b5cfb0c62634c4cef2101e8451737b5
Harr-izle <harrietbuadee@gmail.com>
2024-12-11 08:07:42 +0000

    feat:renders quote and call dynamically

--- src/app/features/service-provider/pages/calls/calls.component.ts
+++ src/app/features/service-provider/pages/calls/calls.component.ts
@@ -3,6 +3,10 @@ import { Component, HostListener } from '@angular/core';
 import { TableModule } from 'primeng/table';
 import { calls } from '../../data';
 import { Statuses } from '../../../../utils/status';
+import { CallService } from '../../../service-discovery/services/call/call.service';
+import { CallData } from '../../../service-discovery/models/call';
+import { callData } from '../../models/callData';
+
 
 @Component({
   selector: 'app-calls',
@@ -12,24 +16,49 @@ import { Statuses } from '../../../../utils/status';
   styleUrl: './calls.component.sass'
 })
 export class CallsComponent {
-  calls = calls
+  
+  calls:callData[]=[]
 
    statuses = Statuses
 
-  activeQuote: any = null; 
+  activeCall: any = null; 
 
-  toggleMenu(quote: any) {
-    this.activeQuote = this.activeQuote === quote ? null : quote;
+  constructor(private callService:CallService){}
+
+ 
+  ngOnInit() {
+    this.fetchCallRequests();
   }
 
-
-  updateStatus(quote: any, status: string) {
-    quote.status = status;
-    this.activeQuote = null; 
+  fetchCallRequests() {
+    this.callService.getCallRequest().subscribe({
+      next: (response) => {
+        this.calls = response;
+      },
+      error: (err) => {
+        console.error('Error fetching call requests:', err);
+      },
+    });
   }
+
+  toggleMenu(call: any) {
+    this.activeCall = this.activeCall === call ? null : call;
+  }
+
+  updateStatus(call: any, status: string) {
+    this.callService.changeStatus(call.id, status).subscribe({
+      next: () => {
+        call.status = status; 
+        this.activeCall = null; 
+      },
+      error: (err) => {
+        console.error('Error updating status:', err);
+      },
+    });
+  }
+
   @HostListener('document:click', ['$event'])
   closeMenu(event: Event) {
-    this.activeQuote = null;
+    this.activeCall = null;
   }
-
 }
