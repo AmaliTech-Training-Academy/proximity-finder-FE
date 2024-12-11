dd31fd2fbee160ffc08820dfa4c9f4aa7a5147ae
Harr-izle <harrietbuadee@gmail.com>
2024-12-11 08:06:23 +0000

    feat:creates interfaces for call and quote data

--- src/app/features/service-discovery/models/call.ts
+++ src/app/features/service-discovery/models/call.ts
@@ -2,3 +2,13 @@ export interface Call{
     phoneNumber:number
     providerEmail:string
 }
+
+export interface CallData{
+    phoneNumber: number;
+    clientEmail:string
+    clientName:string
+    status:string
+    requestDate:string
+    email:string
+    id?:number
+}
