90e0064e44effadb10de3d46d3ffa2c8ea7f8d9a
IT Compliance <rhodaarthur48@gmail.com>
2024-12-12 13:31:06 +0000

    feat: enhance feedback component with client preview and pagination

--- src/app/features/user/components/feedback/feedback.component.ts
+++ src/app/features/user/components/feedback/feedback.component.ts
@@ -3,21 +3,25 @@ import { RatingModule } from 'primeng/rating';
 import { FormsModule } from '@angular/forms';
 import { ReviewService } from '../../../reviews-feedback/services/review.service';
 import { review } from '../../../reviews-feedback/models/ireview';
-import { Observable, Subscription } from 'rxjs';
-
+import { Subscription } from 'rxjs';
+import { PreviewService } from '../../../../core/services/preview.service';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-feedback',
   standalone: true,
-  imports: [ RatingModule, FormsModule],
+  imports: [RatingModule, FormsModule, CommonModule],
   templateUrl: './feedback.component.html',
   styleUrl: './feedback.component.sass'
 })
 export class FeedbackComponent implements OnInit, OnDestroy {
-  value: number = 4
   reviews: review[] = []
   reviewSubscription: Subscription | null = null
+  defaultImage = 'assets/images/default-avatar.png'
+  currentPage = 0
+  reviewsPerPage = 3
+  displayedReviews: review[] = []
 
-  constructor(private reviewService: ReviewService) {}
+  constructor(private reviewService: ReviewService, private previewService: PreviewService) {}
 
   ngOnInit() {
     this.loadReviews()
@@ -28,7 +32,37 @@ export class FeedbackComponent implements OnInit, OnDestroy {
 
     this.reviewService.reviews$.subscribe((data) => {
       this.reviews = data.result
+
+      this.reviews.forEach((review) => {
+        this.previewService.getClientPreview(review.userEmail).subscribe({
+          next: (info) => {
+            review.authorInfo = info
+          }
+        })
+      })
+      this.updateDisplayedReviews()
     })
+
+  }
+
+  updateDisplayedReviews() {
+    const start = this.currentPage * this.reviewsPerPage;
+    const end = start + this.reviewsPerPage;
+    this.displayedReviews = this.reviews.slice(start, end);
+  }
+
+  nextPage() {
+    if ((this.currentPage + 1) * this.reviewsPerPage < this.reviews.length) {
+      this.currentPage++;
+      this.updateDisplayedReviews();
+    }
+  }
+  
+  previousPage() {
+    if (this.currentPage > 0) {
+      this.currentPage--;
+      this.updateDisplayedReviews();
+    }
   }
 
   ngOnDestroy() {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     baseUrl :'http://54.91.220.36:8080/api',
     registration:'https://api.proximity-finder.amalitech-dev.net/api/v1/provider-service/',
-    searchUrl:'http://3.136.48.244:8080/api/v1',
+    searchUrl:'http://113.29.247.162:8080/api/v1',
     faq:'https://api.proximity-finder.amalitech-dev.net/api/v1/support/',
     quote:'https://api.proximity-finder.amalitech-dev.net/api/v1/quote-service/quotes',
 
