06f158a1fc2482ff19e6d874f870d1580c800f7c
IT Compliance <rhodaarthur48@gmail.com>
2024-12-12 09:06:57 +0000

    feat: implement review loading in feedback component

--- src/app/features/user/components/feedback/feedback.component.ts
+++ src/app/features/user/components/feedback/feedback.component.ts
@@ -1,16 +1,39 @@
-import { Component } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RatingModule } from 'primeng/rating';
 import { FormsModule } from '@angular/forms';
-import { ReviewFormComponent } from "../../../reviews-feedback/components/review-form/review-form.component";
+import { ReviewService } from '../../../reviews-feedback/services/review.service';
+import { review } from '../../../reviews-feedback/models/ireview';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-feedback',
   standalone: true,
-  imports: [MatIconModule, RatingModule, FormsModule],
+  imports: [ RatingModule, FormsModule],
   templateUrl: './feedback.component.html',
   styleUrl: './feedback.component.sass'
 })
-export class FeedbackComponent {
-  value: number = 4;
+export class FeedbackComponent implements OnInit, OnDestroy {
+  value: number = 4
+  reviews: review[] = []
+  reviewSubscription: Subscription | null = null
+
+  constructor(private reviewService: ReviewService) {}
+
+  ngOnInit() {
+    this.loadReviews()
+  }
+
+  loadReviews() {
+    this.reviewService.getReviews()
+
+    this.reviewService.reviews$.subscribe((data) => {
+      this.reviews = data.result
+    })
+  }
+
+  ngOnDestroy() {
+    if (this.reviewSubscription) {
+      this.reviewSubscription.unsubscribe()
+    }
+  }
 }
