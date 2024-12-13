f84207e7878bfb204b7d5ad0502eaabe390edbe3
Harr-izle <harrietbuadee@gmail.com>
2024-12-13 09:05:50 +0000

    feat:updates calldata

--- src/app/features/service-discovery/models/call.ts
+++ src/app/features/service-discovery/models/call.ts
@@ -12,3 +12,42 @@ export interface CallData{
     email:string
     id?:number
 }
+
+export interface Sort {
+    sorted: boolean;
+    empty: boolean;
+    unsorted: boolean;
+}
+
+export interface Pageable {
+    pageNumber: number;
+    pageSize: number;
+    sort: Sort;
+    offset: number;
+    paged: boolean;
+    unpaged: boolean;
+}
+
+export interface RequestContent {
+    requestId: number;
+    clientName: string;
+    phoneNumber: string;
+    clientEmail: string;
+    status: string;
+    assignedProvider: string;
+    requestDate: string;
+}
+
+export interface PaginatedRequests {
+    content: RequestContent[];
+    pageable: Pageable;
+    last: boolean;
+    totalPages: number;
+    totalElements: number;
+    first: boolean;
+    size: number;
+    number: number;
+    sort: Sort;
+    numberOfElements: number;
+    empty: boolean;
+}
