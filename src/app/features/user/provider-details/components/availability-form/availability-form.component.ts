03f171005ad71b3df3b2b80f8ab63dbb9e852778
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-12-13 08:58:15 +0000

    Implements service provider booking

--- src/app/features/user/provider-details/components/availability-form/availability-form.component.ts
+++ src/app/features/user/provider-details/components/availability-form/availability-form.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
-import { PasswordInputComponent } from "../../../../profile-management/components/password-input/password-input.component";
+import { Component, Input, OnInit } from '@angular/core';
+import { PasswordInputComponent } from '../../../../profile-management/components/password-input/password-input.component';
+import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
+import { BookingFormComponent } from '../../../../seeker/components/booking-form/booking-form.component';
 
 @Component({
   selector: 'app-availability-form',
   standalone: true,
-  imports: [PasswordInputComponent],
+  imports: [
+    PasswordInputComponent,
+    ButtonModule,
+    DialogModule,
+    BookingFormComponent,
+  ],
   templateUrl: './availability-form.component.html',
-  styleUrl: './availability-form.component.sass'
+  styleUrl: './availability-form.component.sass',
 })
-export class AvailabilityFormComponent {
-  isProAvailable = false;
+export class AvailabilityFormComponent implements OnInit {
+  @Input() providerEmail!: string;
+  isProAvailable = true;
+
+  visible: boolean = false;
+
+  ngOnInit(): void {
+    console.log(this.providerEmail);
+  }
+
+  showDialog() {
+    this.visible = true;
+  }
+
+  onCloseDialogClicked(visible: boolean) {
+    this.visible = visible;
+  }
 }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -11,4 +11,6 @@ export const environment = {
   geminiAiKey: 'AIzaSyCM8_y75mvwbhOZP_jW7ZW1-R0Ug4W8V20',
   quote:
     'https://api.proximity-finder.amalitech-dev.net/api/v1/quote-service/quotes',
+  quoteServiceUrl:
+    'https://api.proximity-finder.amalitech-dev.net/api/v1/quote-service',
 };
