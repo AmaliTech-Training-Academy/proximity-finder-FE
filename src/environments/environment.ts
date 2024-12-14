9b9507bfdcd9596d73ea8244ad8727890fffb40c
Harr-izle <harrietbuadee@gmail.com>
2024-12-14 19:01:12 +0000

    feat:updates apis

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,11 +2,15 @@ export const environment = {
   production: true,
   baseUrl: 'http://54.91.220.36:8080/api',
   serviceUrl: 'http://113.29.247.162:8080',
-  registration:'https://api.proximity-finder.amalitech-dev.net/api/v1/provider-service/',
+  // registration:'https://api.proximity-finder.amalitech-dev.net/api/v1/provider-service/',
+  registration:'http://113.29.247.162:8888/api/v1/provider-service/',
   searchUrl: 'http://113.29.247.162:8080/api/v1',
-  faq: 'https://api.proximity-finder.amalitech-dev.net/api/v1/support/',
+  // faq: 'https://api.proximity-finder.amalitech-dev.net/api/v1/support/',
+  faq: 'http://113.29.247.162:8888/api/v1/support/',
 
   geminiAiKey: 'AIzaSyCM8_y75mvwbhOZP_jW7ZW1-R0Ug4W8V20',
+  // quote:
+  //   'https://api.proximity-finder.amalitech-dev.net/api/v1/quote-service',
   quote:
-    'https://api.proximity-finder.amalitech-dev.net/api/v1/quote-service',
+    'http://113.29.247.162:8888/api/v1/quote-service',
 };
